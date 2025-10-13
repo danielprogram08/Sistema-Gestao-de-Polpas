d219d7324e0f0e69990b5fa71773e92435777fdc
danielprogram08 <danieldanielsilva08@gmail.com>
2025-10-13 00:42:53 -0300

    mod: modify Ambient Back and DB

---
+++ BACK/hoopoes-management-system/src/main/java/com/sgp/hoopoes_management_system/HoopoesManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.sgp.hoopoes_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopoesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopoesManagementSystemApplication.class, args);
+	}
+}
---
+++ BACK/hoopoes-management-system/src/test/java/com/sgp/hoopoes_management_system/HoopoesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgp.hoopoes_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoopoesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
