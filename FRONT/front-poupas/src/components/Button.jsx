d45538fa772a0ed774144d5e2881c1ca4f8690a6
caiovtech <caiovtech@outlook.com>
2025-10-29 16:50:28 -0300

    Mod: Alteração nos Componentes Button e Header

--- FRONT/front-poupas/src/components/Button.jsx
+++ FRONT/front-poupas/src/components/Button.jsx
@@ -1,6 +1,12 @@
-export default function Button({children}) {
+export default function Button({children, iconOnly = false}) {
+    
+    const baseClasses = "flex items-center pt-3.5 pb-3.5 pl-10 pr-10 bg-[#B78F57] rounded-[5px] text-white font-semibold cursor-pointer";
+    const iconClasses = "p-0 bg-transparent cursor-pointer";
+    const finalClasses = iconOnly ? iconClasses : baseClasses;
+
+
     return (
-        <button className="flex items-center pt-3.5 pb-3.5 pl-10 pr-10 bg-[#B78F57] rounded-[5px] text-white font-semibold cursor-pointer">
+        <button className={finalClasses}>
             {children}
         </button>
     )
--- FRONT/front-poupas/src/components/Header.jsx
+++ FRONT/front-poupas/src/components/Header.jsx
@@ -1,16 +1,26 @@
+import Button from './Button';
+
+import { FaUserCircle } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { TbReportAnalytics } from "react-icons/tb";
+import { FaBell } from "react-icons/fa";
+
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-[#111418]/95 backdrop-blur-sm w-full border-b border-[#2e3237]">
+        <header className="sticky top-0 z-50 bg-[#111418]/95 backdrop-blur-sm w-full border-b border-[#2e3237] h-20 flex items-center">
 
             <div className="w-full flex h-16 items-center justify-between">
-                <div className="w-[33%] bg-amber-500">
-                    <p>1</p>
+                <div className="flex justify-center w-[25%]">
+                    <h1 className="text-white text-[35px]">Casa Bessa Polpas</h1>
                 </div>
-                <div className="w-[33%] bg-blue-500">
-                    <p>2</p>
+                <div className="flex justify-center w-[50%]">
+                    <input type="search" placeholder="Pesquisar polpa" className="bg-white w-[65%] rounded-xs h-8"></input>
                 </div>
-                <div className="w-[33%] bg-green-600">
-                    <p>3</p>
+                <div className="flex justify-around w-[25%]">
+                    <Button iconOnly><FaUserCircle color="white" size="25px" /></Button>
+                    <Button iconOnly><AiFillHome color="white" size="25px" /></Button>
+                    <Button iconOnly><TbReportAnalytics color="white" size="25px" /></Button>
+                    <Button iconOnly><FaBell color="white" size="25px" /></Button>
                 </div>
             </div>
         </header>
