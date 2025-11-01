974aa41019076971c54d8c0be3adf75e7fab7c99
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-11-01 16:39:53 -0300

    fix: color pagewrapper

--- FRONT/front-poupas/src/layouts/AuthLayout.jsx
+++ FRONT/front-poupas/src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 function AuthLayout() {
   return (
-    <div className="min-h-screen flex bg-[#e4e4e4]">
+    <div className="min-h-screen flex bg-[#d6d6d6]">
       <div className="w-1/2 bg-linear-to-r from-blue-500 to-purple-600 flex flex-col justify-center items-center text-white p-12 rounded-2xl">
         <h1 className="text-4xl font-bold mb-4">Sistema de Gestão de Poupas</h1>
         <p className="text-lg">A sua solução completa para gerenciamento de poupas.</p>
